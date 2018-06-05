7127425e5f68eaca29723594c0314d5206db7d15
Ibone R. González Mauraza <Ibone.Rosa@gmail.com>
2018-06-05 09:09:11 +0200

    little changes

--- src/backend/service/DefaultService.js
+++ src/backend/service/DefaultService.js
@@ -6,6 +6,8 @@ const request = require("request");
 const Promise = require("bluebird");
 const Engine = require("json-rules-engine").Engine;
 
+const logger = require("../logger");
+
 
 /**
  * Return..
@@ -17,103 +19,90 @@ exports.getBilling = function (rules) {
   return new Promise(function (resolve, reject) {
     var engine = new Engine();
     var examples = {};
-    examples["application/json"] = {
-      from: "2000-01-23",
-      rules: [{
-          conditions: {
-            all: [{
-                fact: "fact",
-                value: "value",
-                operator: "operator"
-              },
-              {
-                fact: "fact",
-                value: "value",
-                operator: "operator"
-              }
-            ]
-          },
-          event: {
-            params: "params",
-            type: "type"
-          }
-        },
-        {
-          conditions: {
-            all: [{
-                fact: "fact",
-                value: "value",
-                operator: "operator"
-              },
-              {
-                fact: "fact",
-                value: "value",
-                operator: "operator"
-              }
-            ]
-          },
-          event: {
-            params: "params",
-            type: "type"
-          }
-        }
-      ],
-      metrics: [{
-          name: "name",
-          id: 0,
-          url: "url"
-        },
-        {
-          name: "name",
-          id: 0,
-          url: "url"
-        }
-      ],
-      to: "2000-01-23"
-    };
+
     // if (Object.keys(examples).length > 0) {
     //   resolve(examples[Object.keys(examples)[0]]);
-    var newFact = {};
     engine.on("success", (event, almanac) => {
-      console.log("System" + " have ".green + "....." + event.type.underline + " " + event.params.message);
-      if (event.type == "incremment-fact") {
-        almanac.factMap.get(event.params.fact).value = almanac.factMap.get(event.params.fact).value + event.params.value;
-        // almanac.factMap.set(event.params.fact, aux);
-        // result.billing = almanac.factMap.get(event.params.fact).value;
-        // result.messages.push(event.type + ' - ' + event.params.message);
-        console.log('REWARD: ' + almanac.factMap.get(event.params.fact).value);
-      }
-    })
+      logger.info("System" + " have ".green + "....." + event.type.underline + " " + event.params.message);
+      // if (event.type == "increment-fact") {
+      //   almanac.factMap.get(event.params.fact).value = almanac.factMap.get(event.params.fact).value + event.params.value;
+      // almanac.factMap.set(event.params.fact, aux);
+      // result.billing = almanac.factMap.get(event.params.fact).value;
+      // result.messages.push(event.type + ' - ' + event.params.message);
+      logger.info('REWARD: ' + event.params.value);
+      // }
+    });
     engine.on('failure', event => {
-      console.log('System' + ' haven´t '.red + ' ..... ' + event.type.underline + ' ' + event.params.message);
+      logger.info('System' + ' haven´t '.red + ' ..... ' + event.type.underline + ' ' + event.params.message);
     });
     rules.rules.forEach(newRule => {
-      console.log('newRule ' + JSON.stringify(newRule));
+      logger.info('New rule: ' + JSON.stringify(newRule));
       engine.addRule(newRule);
     });
-    rules.metrics
-      .forEach(newMetric => {
-        console.log('newMetric ' + JSON.stringify(newMetric));
-        var name = newMetric.name;
-        var api = getApi(newMetric.url, rules.from, rules.to).then(value => {
-          newFact = JSON.parse('{ "reward": 0, "' + name + '": ' + value + '}');
-          console.log("newFact: " + JSON.stringify(newFact));
-          var reward = 0;
-          engine.run(newFact).then((event) => {
-            let result = {
-              reward: 0,
-              message: []
-            };
-            event.forEach(function (fact) {
-              result.reward += fact.params.value;
-              result.message.push(fact.params.message);
-            })
-            return resolve(result);
-          });
+
+    buildFacts(rules.metrics, rules.from, rules.to).then(newMetric => {
+      logger.info("metrics: " + JSON.stringify(newMetric));
+      engine.run(newMetric).then((event) => {
+        getResults(event).then(result => {
+          return resolve(result);
         });
       });
+    });
   });
 };
+
+function getResults(event) {
+  return new Promise(function (resolve, reject) {
+    var result = {
+      reward: 0,
+      penalties: 0,
+      message: []
+    };
+    event.forEach(function (fact) {
+      switch (fact.type) {
+        case ("decrement-fact"):
+          result.reward += fact.params.value;
+          result.message.push(fact.params.message)
+          break;
+        case ("increment-fact"):
+          result.penalties += fact.params.value;
+          result.message.push(fact.params.message)
+          break;
+        case ("commitment"):
+          result.message.push(fact.params.message);
+          break;
+        default:
+          break;
+      }
+    });
+    resolve(result);
+  });
+
+}
+
+function buildFacts(metrics, from, to) {
+  return new Promise(function (resolve, reject) {
+    var newFact = '{ "reward": 0 ';
+    var apiResult = [];
+    metrics.forEach(newMetric => {
+      apiResult.push(
+        new Promise((resolve, reject) => {
+          logger.info('newMetric: ' + JSON.stringify(newMetric));
+          var name = newMetric.name;
+          getApi(newMetric.url, from, to).then(value => {
+            newFact = newFact + ', "' + name + '" : ' + value;
+            logger.info("facts: " + newFact);
+            resolve(newFact);
+          });
+        }));
+    });
+    Promise.all(apiResult).then(value => {
+      value = value[value.length - 1] + '}';
+      value = JSON.parse(value);
+      resolve(value);
+    });
+  });
+}
 
 function getApi(url, from, to) {
   return new Promise(function (resolve, reject) {
@@ -121,6 +110,7 @@ function getApi(url, from, to) {
     if (to) {
       myUrl = myUrl + "&to=" + to;
     }
+    logger.info("URL: " + myUrl);
     request({
         method: "GET",
         url: myUrl,
@@ -128,11 +118,15 @@ function getApi(url, from, to) {
       },
       (err, res, body) => {
         if (err) {
-          console.log(err);
+          logger.info(err);
           reject(err);
         }
-        // console.log(res.statusCode);
-        resolve(body.response.value);
+        // logger.info(res.statusCode);
+        if (body.response.value) {
+          resolve(body.response.value);
+        } else {
+          resolve(body.response);
+        }
       }
     );
   });
